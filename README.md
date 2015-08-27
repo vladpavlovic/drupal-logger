a4a0e90eeee8f313491fc2cb0c8ad277d0910664
mundanity <owen@mundanity.com>
2015-08-27 08:36:10 -0700

    Add an optional log level to ignore events at or below.

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ A simple PSR-3 implementation of a logger for Drupal watchdog calls.
 
 The PSR-3 parameter ```$context``` is passed to watchdog as variables, for use as placeholders.
 
-```
     class MyClass
     {
         public function myMethod($param)
@@ -14,6 +13,15 @@ The PSR-3 parameter ```$context``` is passed to watchdog as variables, for use a
             return $param;
         }
     }
-```
 
 The watch dog type is set as the function or class method that called the logging code. In the example above, the watchdog type is set as *MyClass::myMethod*.
+
+## Controlling log levels
+
+Log levels below a specified level can be ignored, which may help reduce noise in production systems. When the class is initiated, an optional LogLevel may be provided. Events at or below the provided LogLevel will be ignored.
+    $logger = new WatchdogLogger(LogLevel::NOTICE);
+
+    // This debug message will be ignored.
+    $logger->debug('Some debugging information');
+
+By default, no events are ignored.
--- src/WatchdogLogger.php
+++ src/WatchdogLogger.php
@@ -12,6 +12,29 @@ use Psr\Log\AbstractLogger;
  */
 class WatchdogLogger extends AbstractLogger
 {
+    /**
+     * The minimum level of logging to ignore. All events at or below this level
+     * will be ignored. This is a LogLevel constant.
+     *
+     * @var string
+     *
+     */
+    protected $ignore = null;
+
+
+    /**
+     * Constructor
+     *
+     * @param string $ignore_level
+     *   The minimum level of logging to ignore. All events at or below this
+     *   level will be ignored.
+     *
+     */
+    public function __construct($ignore_level = null)
+    {
+        $this->ignore = $ignore_level;
+    }
+
 
     /**
      * {@inheritdoc}
@@ -30,7 +53,12 @@ class WatchdogLogger extends AbstractLogger
               LogLevel::DEBUG     => WATCHDOG_DEBUG,
         );
 
+        $ignore   = isset($map[$this->ignore]) ? $map[$this->ignore] : null;
         $severity = isset($map[$level]) ? $map[$level] : WATCHDOG_NOTICE;
+
+        if (is_int($ignore) && $severity >= $ignore) {
+          return;
+        }
 
         // This is pretty hacky. Basically, we want to know what called the log
         $trace    = debug_backtrace();
